0631f11d6b9641131975b25ecbaf848052769de1
Eskindir <eskendiraleb@gmail.com>
2021-05-02 01:26:07 -0500

    Alex first contribution in Indu scuad

---
+++ src/main/java/InduScuad/com/restaurantfinders/RestaurantfindersApplication.java
@@ -0,0 +1,13 @@
+package InduScuad.com.restaurantfinders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantfindersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantfindersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/InduScuad/com/restaurantfinders/RestaurantfindersApplicationTests.java
@@ -0,0 +1,13 @@
+package InduScuad.com.restaurantfinders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantfindersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
